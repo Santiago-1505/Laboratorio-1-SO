16f8e5152b0f02e5830265375f9312f118663420
Santiago-1505 <santiagovillegas853@gmail.com>
2026-03-12 17:44:16 -0500

    Solución al punto 2 wgrep

--- wgrep/main.c
+++ wgrep/main.c
@@ -1,34 +1,63 @@
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+void wgrepStdin(char *searchTerm);
+void wgrepFile(char *searchTerm, char *fileName);
 int main(int argc, char *argv[])
 {
-    FILE *stream;
-    char *line = NULL;
-    size_t size = 0;
-    ssize_t nread;
-
     if (argc < 2)
     {
         printf("wgrep: searchterm [file ...]\n");
         exit(EXIT_FAILURE);
     }
 
-    stream = fopen(argv[1], "r");
-    if (stream == NULL)
+    char *searchTerm = argv[1];
+
+    if (argc == 2){
+        wgrepStdin(searchTerm);
+        exit(EXIT_SUCCESS);
+    }
+    for (int i = 2; i < argc; i++){
+        wgrepFile(searchTerm, argv[i]);
+    }
+
+    exit(EXIT_SUCCESS);
+}
+
+void wgrepStdin(char *searchTerm){
+    char *line = NULL;
+    size_t size = 0;
+
+    while (getline(&line, &size, stdin) != -1)
     {
-        perror("fopen");
+        if (strstr(line, searchTerm) != NULL)
+        {
+            printf("%s", line);
+        }
+    }
+    free(line);
+}
+
+void wgrepFile(char *searchTerm, char *fileName){
+    FILE *fptr;
+    fptr = fopen(fileName, "r");
+    if (fptr == NULL)
+    {
+        printf("wgrep: cannot open file %s\n", fileName);
         exit(EXIT_FAILURE);
     }
 
-    while ((nread = getline(&line, &size, stream)) != -1)
-    {
-        printf("Retrieved line of length %zd:\n", nread);
-        fwrite(line, nread, 1, stdout);
-    }
+    char *line = NULL;
+    size_t size = 0;
 
+    while (getline(&line, &size, fptr) != -1)
+    {
+        if (strstr(line, searchTerm) != NULL)
+        {
+            printf("%s", line);
+        }
+    }
     free(line);
-    fclose(stream);
-    exit(EXIT_SUCCESS);
+    fclose(fptr);
 }
